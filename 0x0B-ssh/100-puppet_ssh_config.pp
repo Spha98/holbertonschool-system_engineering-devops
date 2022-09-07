6d79690721d530cd2c1b2769ead54c0f41866bdf
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-09-07 11:05:22 +0200

    Fixing files

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
-# set up your client SSH configuration file so that you can connect to a server without typing a password.
-include stdlib
+#!/usr/bin/env bash
+# connect with puppet
 
 file_line { 'Use private key':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
 }
 
-file_line { 'Refuse password':
+file_line { 'Turn_off_password_auth':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
